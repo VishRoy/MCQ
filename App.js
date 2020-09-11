6910cb960857092a63649d3fac1a95d65c73fb68
VishRoy <69618079+VishRoy@users.noreply.github.com>
2020-09-11 22:06:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  StatusBar,
+} from 'react-native';
+
+import WelcomeScreen from './src/components/welcomeScreen';
+import QuizScreen from './src/components/quiz';
+import ScoreCardScreen from './src/components/scorecard';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <>
+    <StatusBar barStyle="dark-content" />
+     <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={WelcomeScreen} />
+        <Stack.Screen name="Quiz" component={QuizScreen} />
+        <Stack.Screen name="Score" component={ScoreCardScreen} /> 
+      </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+
+
+export default App;
