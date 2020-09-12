517533ee71b23c6143a71c2cdbcf8a9ffd6c499e
VishRoy <69618079+VishRoy@users.noreply.github.com>
2020-09-12 10:49:23 +0530

    Removed unused variables

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef , useEffect} from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import quizQuestions from '../json/quizQuestions';
 import QuestionComponent from './questions';
